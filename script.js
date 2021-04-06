44d560728e143ded9b768afc7cb8980aafe65a26
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-04-06 16:04:50 -0400

    Created API call to return data for current weather and forecasted weather.

--- script.js
+++ script.js
@@ -16,13 +16,14 @@ let currentHumidity;
 let currentWind;
 let currentUV;
 let forecast;
-
+let long;
+let lat;
 // Set current search to local storage
 function setCity(event) {
     event.preventDefault();
     var city = document.getElementById("city-name").value;
     localStorage.setItem('location', city);
-    alert(city + " executed.");
+    console.log(city + " executed.");
 }
 
 // Not console.logging or appending the li
@@ -32,69 +33,34 @@ function appendCity(event) {
     var newButton = document.createElement('button');
     newButton.textContent = document.getElementById("city-name").value;
     ul.appendChild(newButton);
+    newButton.setAttribute('style', 'padding: 5px; text-align: left;')
     console.log('Executed appendCity');
 }
-
-// function addDate() {
-//     document.getElementById('current-city').innerHTML = todaysDate;
-// }
-// addDate();
 
 button.addEventListener("click", setCity);
 button.addEventListener("click", appendCity);
 
-// Not doing jack....
 /* API call for city's weather data */
-// var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid=ed5a824dd1bf2f56ad85d4425eb7d971&units=imperial';
-
-// button.addEventListener('click', function () {
-//     fetch(requestUrl)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data)
-//         });
-// });
-
-// TEST
-// button.addEventListener('click', function () {
-//     fetch('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=ed5a824dd1bf2f56ad85d4425eb7d971&units=imperial')
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         });
-// });
-
 button.addEventListener('click', function (event) {
     event.preventDefault();
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + document.getElementById("city-name").value + '&appid=ed5a824dd1bf2f56ad85d4425eb7d971&units=imperial')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + document.getElementById("city-name").value + '&appid=ed5a824dd1bf2f56ad85d4425eb7d971&units=imperial')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            // console.log(data.city.name);
+            console.log(data.coord.lon + ' ' + data.coord.lat);
+            long = data.coord.lon;
+            lat = data.coord.lat;
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&exclude={part}&appid=' + API_KEY)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
         });
+
 });
-
-
-// / button.addEventListener('click', function () {
-//     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid=ed5a824dd1bf2f56ad85d4425eb7d971&units=imperial')
-//     alert(inputValue.value)
-//         // .then(response => response.json())
-//         // .then(data => console.log(data))
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         });
-// });
-
-
-
-
-
 
