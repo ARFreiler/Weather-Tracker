958537bed7335c03f573edd3809a86aea6570255
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-04-10 16:35:00 -0400

    Adjusted forecast elements to display proper five day dates, icons, and humidity stats. Corrected current weather element to show weather icon.

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function appendCity(event) {
     event.preventDefault();
     var ul = document.querySelector('#search-history')
     var newButton = document.createElement('button');
-    newButton.textContent = document.getElementById("city-name").value;
+    newButton.textContent = document.getElementById("city-name").value.charAt(0).toUpperCase() + document.getElementById("city-name").value.slice(1);;
     ul.appendChild(newButton);
 }
 
@@ -60,7 +60,6 @@ button.addEventListener('click', function (event) {
         })
         .then(function (data) {
             console.log(data);
-            // console.log(data.coord.lon + ' ' + data.coord.lat);
             long = data.coord.lon;
             lat = data.coord.lat;
             city = data.name;
@@ -69,12 +68,7 @@ button.addEventListener('click', function (event) {
             cityWind = data.wind.speed;
             currentIcon = data.weather[0].icon;
             iconElement = document.createElement("img");
-            iconElement.setAttribute("src", "http://openweathermap.org/img/wn/" + currentIcon + ".png");
-            // currentIcon = data.weather[0].icon;
-            // console.log(city);
-            // console.log(cityTemp);
-            // console.log(cityHumidity);
-            // console.log(cityWind);
+            iconElement.setAttribute("src", "http://openweathermap.org/img/w/" + currentIcon + ".png");
             fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&exclude={part}&appid=' + API_KEY + '&units=imperial')
                 .then(function (response) {
                     return response.json();
@@ -82,31 +76,32 @@ button.addEventListener('click', function (event) {
                 .then(function (data) {
                     console.log(data);
                     cityUV = data.current.uvi;
-                    unixTime = data.daily[1].dt;
-                    unixMilliSeconds = unixTime * 1000;
-                    forecastDateObject = new Date(unixMilliSeconds);
-                    forecastDate = forecastDateObject.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" })
+
                     let forecastElements = document.querySelectorAll('.forecast');
                     for (let i = 0; i < forecastElements.length; i++) {
                         forecastElements[i].innerHTML = '';
                         let forecastIndex = data.daily[i];
+                        unixTime = data.daily[i + 1].dt;
+                        unixMilliSeconds = unixTime * 1000;
+                        forecastDateObject = new Date(unixMilliSeconds);
+                        forecastDate = forecastDateObject.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" })
                         let forecastDateElement = document.createElement('p');
                         forecastDateElement.innerHTML = forecastDate;
                         forecastElements[i].append(forecastDateElement);
-                        console.log(forecastDate);
                         let forecastWeatherElement = document.createElement("img");
-                        forecastWeatherElement.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
+                        forecastWeatherElement.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + ".png");
                         forecastElements[i].append(forecastWeatherElement);
                         let forecastTempElement = document.createElement('p');
-                        forecastTempElement.innerHTML = "Temp: " + data.daily[i].temp.day + " &#176F";
+                        forecastTempElement.innerHTML = "Temp: " + data.daily[i + 1].temp.day + " &#176F";
                         forecastElements[i].append(forecastTempElement);
                         let forecastHumidityElement = document.createElement('p');
-                        forecastHumidityElement.innerHTML = "Humidity: " + cityHumidity + "%";
+                        forecastHumidityElement.innerHTML = "Humidity: " + data.daily[i + 1].humidity + "%";
                         forecastElements[i].append(forecastHumidityElement);
                     }
                     document.getElementById('uvi').innerHTML = "UV Index: " + cityUV;
                 })
-            document.getElementById('current-city').innerHTML = city + todaysDate.format(' ' + '(MMMM/Do/YYYY)') + currentIcon;
+            document.getElementById('current-city').innerHTML = city + todaysDate.format(' ' + '(MMMM/Do/YYYY)');
+            currentCity.appendChild(iconElement);
             document.getElementById('temp').innerHTML = "Temperature: " + cityTemp + "&#176F";
             document.getElementById('humidity').innerHTML = "Humidity: " + cityHumidity + "%";
             document.getElementById('wind').innerHTML = "Wind Speed: " + cityWind + " MPH";
